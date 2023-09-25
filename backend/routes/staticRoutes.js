44b9290361e6bf5c986bc3b5be7b7d88aaf13d0a
Marco Coppola <marcocoppola02.mc@gmail.com>
2023-09-25 12:25:41 +0200

    Removed mongodb-data

--- backend/routes/staticRoutes.js
+++ backend/routes/staticRoutes.js
@@ -7,5 +7,11 @@ router.get('/auth', (req, res) => {
     res.sendFile(path.join(__dirname,'../../frontend/shared/auth/auth.html'));
 });
 
+// Axios lib
+router.get('/axios', (req, res) => {
+    res.sendFile(path.join(__dirname,'../../frontend/shared/js/axios.min.js'));
+});
+
+
 
 module.exports = router;
