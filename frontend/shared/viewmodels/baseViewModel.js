71364496b9ac0fb9ca280ce3c5bf308232a02b0a
Murkrow <marcocoppola02.mc@gmail.com>
2023-09-27 17:01:00 +0200

    Added redirect to login if unauthorized

--- frontend/shared/viewmodels/baseViewModel.js
+++ frontend/shared/viewmodels/baseViewModel.js
@@ -1,3 +1,15 @@
-//This file needs to be imported before all other view models
-axios.defaults.baseURL = '/api';
-axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token');
+let api = axios.create({
+    baseURL: '/api',
+    timeout: 1000,
+    headers: {'Authorization': "Bearer " + localStorage.getItem('token')}
+});
+
+// This is triggered on each response, if unauthorized redirect to login
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 401) {
+            alert("Non sei autorizzato ad accedere a questa pagina, verrai reindirizzato alla pagina di login");
+            window.location.href = '/static/auth';
+        }
+    });
--- frontend/shared/viewmodels/squealViewModel.js
+++ frontend/shared/viewmodels/squealViewModel.js
@@ -1,5 +1,16 @@
 
 //Note: this is used only for debug purposes
 function getAllSqueals(){
-    return axios.get("squeals");
+    return api.get("squeals");
+}
+
+function postSqueal(squeal, channels){
+
+    //Create mixed object
+    let post = {
+        squeal: squeal,
+        channels: channels
+    }
+
+    return api.post("squeals", post);
 }
