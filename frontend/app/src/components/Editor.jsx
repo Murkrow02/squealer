669b623c8d42ef48ac52820c869b4a9b8a0d525f
Valex <valeriopio02@gmail.com>
2023-10-02 20:34:47 +0200

    Super editor

--- frontend/app/src/components/Editor.jsx
+++ frontend/app/src/components/Editor.jsx
@@ -15,6 +15,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import * as PropTypes from "prop-types";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import "../css/NewSqueal.css";
+import {scryRenderedComponentsWithType} from "react-dom/test-utils";
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -32,6 +33,23 @@ VisuallyHiddenInput.propTypes = {type: PropTypes.string};
 
 const IMAGE_CHAR_SIZE = 125;
 export default function Editor(props) {
+
+    useEffect(() => {
+        let input = document.getElementById("outlined-multiline-flexible");
+        input.addEventListener("select", getTextSelection);
+        input.addEventListener('keypress', checkcaret); // Every character written
+        input.addEventListener('mousedown', checkcaret); // Click down
+        input.addEventListener('touchstart', checkcaret); // Mobile
+        input.addEventListener('input', checkcaret); // Other input events
+        input.addEventListener('paste', checkcaret); // Clipboard actions
+        input.addEventListener('cut', checkcaret);
+        input.addEventListener('mousemove', checkcaret); // Selection, dragging text
+        input.addEventListener('select', checkcaret); // Some browsers support this event
+        input.addEventListener('selectstart', checkcaret); // Some browsers support this event
+
+    }, [])
+
+
 
     //Chars left
     const [maxDayChars, setMaxDayChars] = React.useState(props.day_max);
@@ -57,10 +75,8 @@ export default function Editor(props) {
 
         //check if max chars reached
         if ((initialDayChars - value) < 0 || (initialWeekChars - value) < 0 || (initialMonthChars - value) < 0) {
-
             //check if type is text
             if (squealType === "text") {
-
                 //remove last character from text
                 document.getElementById("outlined-multiline-flexible").value = document.getElementById("outlined-multiline-flexible").value.slice(0, -1);
                 alert("You have reached the maximum number of characters for this squeal type.");
@@ -68,8 +84,6 @@ export default function Editor(props) {
                 alert("Image size is 125 characters so you cannot upload it at this time.");
                 setImage(null);
             }
-
-
             return;
         }
 
@@ -83,6 +97,7 @@ export default function Editor(props) {
         if (squealType !== "text") {
             return;
         }
+
         //update char count
         decreaseCharCount(event.target.value.length);
 
@@ -93,37 +108,199 @@ export default function Editor(props) {
         //split text into array
         const segments = event.target.value.split(" ");
 
+        //iterate over segments
+        for (let i = 0; i < segments.length; i++) {
+            if (segments[i] === '\n') {
+                continue;
+            }
+            //check if segment contains \n
+            if (segments[i].includes("\n")) {
+
+                //split segment into array
+                const lines = segments[i].split("\n");
+                let tmp_seg = [];
+                //iterate over lines
+                for (let j = 0; j < lines.length; j++) {
+                    //push line to tmp array
+                    tmp_seg.push(lines[j]);
+                    //push \n
+                    tmp_seg.push("\n");
+                }
+                //remove last char, if segment ends with \n is empty while if it does not ends with \n is \n
+                tmp_seg.pop();
+                //insert tmp array into segments at index i
+                segments.splice(i, 1, ...tmp_seg);
+            }
+        }
+
+        //check if last segment's last char is \n
+        if (segments[segments.length - 1].endsWith("\n")) {
+            if (segments[segments.length - 1] !== "\n") {
+                //remove last char
+                segments[segments.length - 1] = segments[segments.length - 1].slice(0, -1);
+                //push \n to new segment
+                segments.push("\n");
+            }
+        }
+
+
+
+        let counter = -1;
         //generate HTML
-        const html = segments.map((segment, index) => {
+        let html = segments.map((segment, index) => {
 
+            let selected = false;
+            let selectedFromStart = 0;
+            let selectedFromEnd = 0;
+            let unselectedFromSides = [];
+            //check if user has selected text
+            if (startPos !== -1 && endPos !== -1) {
 
+                if (segment !== "" && segment !== '\n') {
+                    //check if word is fully selected
+                    if (counter + 1 >= startPos && counter + segment.length <= endPos) {
+                        selected = true;
+                    } //first part of word selected
+                    else if (startPos <= counter + 1 && endPos < counter + segment.length) {
+                        selectedFromStart = endPos - counter - 1;
+                    } //last part of word selected
+                    else if (startPos > counter + 1 && endPos >= counter + segment.length) {
+                        selectedFromEnd = counter + segment.length - startPos + 1;
+                    } //middle part of word selected
+                    else if (startPos > counter + 1 && endPos < counter + segment.length) {
+                        unselectedFromSides.push(startPos - counter - 1);
+                        unselectedFromSides.push(counter + segment.length - endPos + 1);
+                    }
+                }
+            }
 
+            /*TODO FIX
+               if (curPos === 0 && index === 0) {
+                segment = "|" + segment;
+            } else if (curPos === counter + segment.length) {
+                segment = segment + "|";
+            }*/
 
+            if (curPos >= counter + 1 && curPos <= counter + segment.length) {
+                //get characters before cursor position
+                let firstChars = segment.substring(0, curPos - counter - 1);
+                //get characters after cursor position
+                let lastChars = segment.substring(curPos - counter - 1, segment.length);
+                segment = firstChars + "|" + lastChars;
+                console.log([firstChars, lastChars]);
+            }
+
+            //increase counter
+            counter += segment.length;
+
+            //whitespace handling
+            if (segment !== '\n') {
+                counter += 1;
+            }
 
             if (segment.match(urlRegex) || segment.match(mentionRegex)) {
-                //TODO FORMAT LINKS with \n
-                return `<span class="highlight">${segment}</span>`
+                //TODO create array of links and mentions
+                if (selectedFromStart > 0) {
+                    let firstChars = segment.substring(0, selectedFromStart);
+                    let lastChars = segment.substring(selectedFromStart, segment.length);
+                    return `<span class="selected highlight">${firstChars}</span><span class="highlight">${lastChars}</span>`
+                } else if (selectedFromEnd > 0) {
+                    //get last 3 chars
+                    let firstChars = segment.substring(0, segment.length - selectedFromEnd);
+                    let lastChars = segment.substring(segment.length - selectedFromEnd, segment.length);
+                    return `<span class="highlight">${firstChars}</span><span class="selected highlight">${lastChars}</span>`
+                } else if (unselectedFromSides.length === 2) {
+                    //get last 3 chars
+                    let firstChars = segment.substring(0, unselectedFromSides[0]);
+                    let middleChars = segment.substring(unselectedFromSides[0], segment.length - unselectedFromSides[1]);
+                    let lastChars = segment.substring(segment.length - unselectedFromSides[1], segment.length);
+                    return `<span class="highlight">${firstChars}</span><span class="selected highlight">${middleChars}</span><span class="highlight">${lastChars}</span>`
+                }
+
+                return `<span class="highlight ${selected ? 'selected' : ''}">${segment}</span>`
             } else if (segment.includes("\n")) {
-                //split segment into array
-                const lines = segment.split("\n");
-                console.log(lines)
-                //generate HTML
-                const bhtml = lines.map((line, index) => {
-                    //check if last line is empty
-                    if (index === lines.length - 1 && line !== "") {
-                        return `<span>${line}</span>`
-                    }
-                    return `<span>${line}</span><br>`
-                }).join(' ');
-                return bhtml;
+                //check if next segment exists
+                if (segments[index + 1] !== undefined && segments[index + 1] !== "") {
+                    return `<br>`
+                }
+                else {
+                    //put whitespace to trigger new line on div
+                    return `<br><span style="color: transparent">*</span>`
+                }
+            }
+            else if (segment === "") {
+                return ``;
             }
             else {
-                return `<span>${segment}</span>`
+
+                if (selectedFromStart > 0) {
+                    let firstChars = segment.substring(0, selectedFromStart);
+                    let lastChars = segment.substring(selectedFromStart, segment.length);
+                    return `<span class="selected">${firstChars}</span><span>${lastChars}</span>`
+                } else if (selectedFromEnd > 0) {
+                    //get last 3 chars
+                    let firstChars = segment.substring(0, segment.length - selectedFromEnd);
+                    let lastChars = segment.substring(segment.length - selectedFromEnd, segment.length);
+                    return `<span>${firstChars}</span><span class="selected">${lastChars}</span>`
+                } else if (unselectedFromSides.length === 2) {
+                    let firstChars = segment.substring(0, unselectedFromSides[0]);
+                    let middleChars = segment.substring(unselectedFromSides[0], segment.length - unselectedFromSides[1]);
+                    let lastChars = segment.substring(segment.length - unselectedFromSides[1], segment.length);
+                    return `<span>${firstChars}</span><span class="selected">${middleChars}</span><span>${lastChars}</span>`
+                }
+
+                return `<span class="${selected ? 'selected' : ''}">${segment}</span>`
             }
         }).join(' ');
 
+        //append cursor to html
+        //html += `<span style="color: var(--primary)">|</span>`;
+
         //update masked content
         document.getElementById("masked-content").innerHTML = html;
+    }
+
+
+
+    let startPos = -1;
+    let endPos = -1;
+
+    let curPos = 0;
+    function checkcaret(event) {
+        const newPos = document.getElementById("outlined-multiline-flexible").selectionStart;
+        if (newPos !== curPos) {
+            console.log('change to ' + newPos);
+            curPos = newPos;
+        }
+        handleSquealTextChange(event);
+    }
+
+    function getTextSelection(event) {
+        startPos = event.target.selectionStart;
+        endPos = event.target.selectionEnd;
+        let selectedText = event.target.value.substring(startPos, endPos);
+        handleSquealTextChange(event);
+        event.target.addEventListener("click", inputClickHandler);
+    }
+
+    let savedSelection = "";
+    function inputClickHandler(event) {
+        //update positions for race condition
+        startPos = event.target.selectionStart;
+        endPos = event.target.selectionEnd;
+        //get selected text
+        let selectedText = event.target.value.substring(startPos, endPos);
+        //check if user nulled the selection or race condition between unselect and click occurred
+        if (selectedText === "" || selectedText === " " || selectedText === savedSelection) {
+            //clear Ui selection
+            startPos = -1;
+            endPos = -1;
+            handleSquealTextChange(event);
+            event.target.removeEventListener("click", inputClickHandler);
+        } else {
+            //save the selection to avoid race condition on next click
+            savedSelection = selectedText;
+        }
     }
 
     function handleSquealTextFocus(event) {
@@ -131,11 +308,9 @@ export default function Editor(props) {
         if (squealType !== "text") {
             return;
         }
-
         if (event.target.value.length === 0) {
             document.getElementById("masked-content").innerText = "";
         }
-
     }
 
     //handle radio button change
@@ -197,6 +372,7 @@ export default function Editor(props) {
                     defaultValue="text"
                     name="row-radio-buttons-group"
                     onChange={handleSquealTypeChange}
+                    select
                 >
                     <FormControlLabel value="text" control={<Radio />} label="Text" />
                     <FormControlLabel value="image" control={<Radio />} label="Image" />
@@ -225,11 +401,12 @@ export default function Editor(props) {
                             <div id="editor-input-mask">
                                 <span id="masked-content">Insert...</span>
                             </div>
-                            <div style={{display:"flex", justifyContent:'center', gap:'15px'}}>
+                            <div style={{display:"flex", marginTop:'20px', justifyContent:'center', gap:'15px'}}>
                                 <div onClick={() => insertSymbol('@')} className={"symbol"}>@</div>
                                 <div onClick={() => insertSymbol('#')} className={"symbol"}>#</div>
                                 <div onClick={() => insertSymbol('§')} className={"symbol"}>§</div>
                             </div>
+                            <button onClick={getTextSelection}>Get selection</button>
                         </div>
 
 
